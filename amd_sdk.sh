c14f3d8bc12cef8d7702c3884b7dfa744f625749
Claudio Kopper <claudio@kopper.info>
2018-02-20 19:34:26 +0100

    make AMD APP SDK download script work again

--- amd_sdk.sh
+++ amd_sdk.sh
@@ -3,8 +3,8 @@
 # Original script from https://github.com/gregvw/amd_sdk/
 
 # Location from which get nonce and file name from
-URL="http://developer.amd.com/amd-accelerated-parallel-processing-app-sdk/"
-URLDOWN="http://developer.amd.com/amd-license-agreement-appsdk/"
+URL="https://developer.amd.com/amd-accelerated-parallel-processing-app-sdk/"
+URLDOWN="https://developer.amd.com/amd-license-agreement-appsdk/"
 
 NONCE1_STRING='name="amd_developer_central_downloads_page_nonce"'
 FILE_STRING='name="f"'
@@ -12,7 +12,7 @@ POSTID_STRING='name="post_id"'
 NONCE2_STRING='name="amd_developer_central_nonce"'
 USER_AGENT='Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0'
 
-FORM=`wget -q -c -t 3 --waitretry=3 --user-agent="$USER_AGENT" -O - $URL | sed -n '/download-2/,/64-bit/p'`
+FORM=`wget -c -t 3 --waitretry=3 --user-agent="$USER_AGENT" -O - $URL | sed -n '/download-2/,/64-bit/p'`
 
 # Get nonce from form
 NONCE1=`echo $FORM | awk -F ${NONCE1_STRING} '{print $2}'`
@@ -26,9 +26,9 @@ POSTID=`echo $POSTID | awk -F'"' '{print $2}'`
 FILE=`echo $FORM | awk -F ${FILE_STRING} '{print $2}'`
 FILE=`echo $FILE | awk -F'"' '{print $2}'`
 
-FORM=`wget -q -c -t 3 --waitretry=3 --user-agent="$USER_AGENT" -O - $URLDOWN --post-data "amd_developer_central_downloads_page_nonce=${NONCE1}&f=${FILE}&post_id=${POSTID}"`
+FORM=`wget -c -t 3 --waitretry=3 --user-agent="$USER_AGENT" -O - $URLDOWN --post-data "amd_developer_central_downloads_page_nonce=${NONCE1}&f=${FILE}&post_id=${POSTID}"`
 
 NONCE2=`echo $FORM | awk -F ${NONCE2_STRING} '{print $2}'`
 NONCE2=`echo $NONCE2 | awk -F'"' '{print $2}'`
 
-wget -q -c -t 3 --waitretry=3 --content-disposition --trust-server-names $URLDOWN --post-data "amd_developer_central_nonce=${NONCE2}&f=${FILE}" --user-agent="$USER_AGENT" -O AMD-SDK.tar.bz2;
+wget -c -t 3 --waitretry=3 --content-disposition --trust-server-names $URLDOWN --post-data "amd_developer_central_nonce=${NONCE2}&f=${FILE}" --user-agent="$USER_AGENT" -O AMD-SDK.tar.bz2;
